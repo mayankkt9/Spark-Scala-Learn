5fc6bec35d48123f566f5cd4712f76ec772cc721
Mayank Kataruka <mayank.kt9@gmail.com>
2020-04-18 02:29:56 -0700

    Spark Notes

--- spark-notes.py
+++ spark-notes.py
@@ -19,9 +19,15 @@ Spark Component
 	- Programming Language Supports (Scala | Java | Python | R)
 	- Storage (LocalFS | HDFS | AmazonS3 | RDBMS | NoSQL)
 
-
-RDD
+ 
+RDD (Resilient Distributed Data)
 Hadoop stored data in multiple distributed storage disk such as HDFS and multiple IO makes it slow.
 Spark came up with In Memory Data Sharing that made it very fast which became 100 times faster than network or disk sharing 
 RDD tried to solve this problem by fault tolerant distributed in-memory computation.
+	- Fundamental Data Structure
+	- All Data are in RDD format
+	- Each Data in RDD can be partitioned logically and can run on parellel on different node on cluster.
+	- Spark takes care of RDD Partition distribution
+	- Highly Resilient (Able to recover quickly as same data chunk are replicated across multiple node)
+
 
