8adfe6fd89d09e146367e3d04741ec020bfa382b
Mayank Kataruka <mayank.kt9@gmail.com>
2020-04-18 02:19:53 -0700

    Spark Notes

---
+++ spark-notes.py
@@ -0,0 +1,20 @@
+What is Spark ? 
+	- Scalable
+	- Massively Parellel
+	- In Memory Execution
+Data can be loaded into memory and analyzed in parellel
+Distribute data in cluster and then analyzed
+Very fast as compared to Hadoop as Spark is IN Memory
+
+Features 
+	- Speed 
+	- Powerful Caching 
+	- Realtime DataAnalysis
+	- Deployment
+	- Polyglot (Scala | Java | Python | R)
+
+Spark Component
+	- Spark Core  (Basic IO | Scheduling | Monitoring | API)
+	- Library (SparkSQL | GraphX | SparkStreaming | SparkMLib)
+	- Programming Language Supports (Scala | Java | Python | R)
+	
