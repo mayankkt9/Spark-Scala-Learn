094a355fe8d8a31c905a6d63204bd4b6b49e93e0
Mayank Kataruka <mayank.kt9@gmail.com>
2020-05-10 13:15:35 -0700

    Spark Scala notes

--- scala-notes.py
+++ scala-notes.py
@@ -6,4 +6,10 @@ Some Features of Scala Programming
 
  Functional Programming is way of writing software application using only pure functions and immutable values.
 
- 
+ library design and data crunching problems (Read later)
+
+
+ What functions are pure functions 
+ 	- Input solely determines the output (There are no other global variable or any other input other than parameter that determines the output)
+ 	- The function doesnt change the input 
+ 	- The function doesnt do anything else except computing the object.
